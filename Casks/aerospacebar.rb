a43742b6c942324fd2f9eca38b9993fcae6fd2f0
Anka <runner@sjc22-bm205-d17b971b-b6c6-482b-9ebb-f6a28a0ea39b-FA124D8AE184.local>
2026-09-04 08:16:54 +0000

    Update AeroSpaceBar to v1.0.0-beta.15

--- Casks/aerospacebar.rb
+++ Casks/aerospacebar.rb
@@ -1,6 +1,6 @@
 cask "aerospacebar" do
-  version "1.0.0-beta.14"
-  sha256 "61203cf9400d0b56e89fbd2286e1889a468a8c83ba124108ba8dabed2abbafcf"
+  version "1.0.0-beta.15"
+  sha256 "7e26f57d75b33aa91f368b04a44aea5be8c82bcd1322f8c51d3dd1e03e99943a"
 
   url "https://github.com/rdrkr/AeroSpaceBar/releases/download/v#{version}/AeroSpaceBar-v#{version}.zip"
   name "AeroSpaceBar"
